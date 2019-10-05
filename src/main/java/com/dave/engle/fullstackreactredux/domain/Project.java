d25184c6aea02333b86b3f88758fbd27ec184b74
Dave Engle <davengle@gmail.com>
2019-10-05 15:25:04 -0400

    Validation foundation

--- src/main/java/com/dave/engle/fullstackreactredux/domain/Project.java
+++ src/main/java/com/dave/engle/fullstackreactredux/domain/Project.java
@@ -1,11 +1,14 @@
 package com.dave.engle.fullstackreactredux.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -19,12 +22,18 @@ public class Project {
     @GeneratedValue
     private Long id;
 
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private LocalDate startDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private LocalDate endDate;
-
     private LocalDateTime created_At;
     private LocalDateTime updated_At;
 
