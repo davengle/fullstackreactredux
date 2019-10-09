45d8095ed035e90352dafa49685b07950744d266
Dave Engle <davengle@gmail.com>
2019-10-09 01:10:28 -0400

    persist project updates

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import Dashboard from "./components/Dashboard";
-import Header from "./components/layout/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/layout/Header";
+import AddProject from "./components/Project/AddProject";
+import UpdateProject from "./components/Project/UpdateProject";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Header />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
+          <Route exact path="/updateProject/:id" component={UpdateProject} />
         </div>
       </Router>
     </Provider>
